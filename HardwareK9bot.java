ce8c16e4800cac94e4edf2dd3e77eeab16107085
RedLionRoboticsClub <32177555+RedLionRoboticsClub@users.noreply.github.com>
2017-11-09 14:59:34 -0500

    Update HardwareK9bot.java

--- HardwareK9bot.java
+++ HardwareK9bot.java
@@ -58,14 +58,14 @@ public class HardwareK9bot
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
     public Servo    arm         = null;
-    //public Servo    claw        = null;
+    public Servo    claw        = null;
 
     public final static double ARM_HOME = 0.2;
-    //public final static double CLAW_HOME = 0.2;
+    public final static double CLAW_HOME = 0.2;
     public final static double ARM_MIN_RANGE  = 0.20;
     public final static double ARM_MAX_RANGE  = 0.90;
-    //public final static double CLAW_MIN_RANGE  = 0.20;
-    //public final static double CLAW_MAX_RANGE  = 0.7;
+    public final static double CLAW_MIN_RANGE  = 0.20;
+    public final static double CLAW_MAX_RANGE  = 0.7;
 
     /* Local OpMode members. */
     HardwareMap hwMap  = null;
@@ -81,8 +81,8 @@ public class HardwareK9bot
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "motorFL");
-        rightDrive = hwMap.get(DcMotor.class, "motorFR");
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Set all motors to zero power
@@ -95,9 +95,9 @@ public class HardwareK9bot
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
-        arm  = hwMap.get(Servo.class, "crservo");
-  //      claw = hwMap.get(Servo.class, "claw");
+        arm  = hwMap.get(Servo.class, "arm");
+        claw = hwMap.get(Servo.class, "claw");
         arm.setPosition(ARM_HOME);
-//        claw.setPosition(CLAW_HOME);
+        claw.setPosition(CLAW_HOME);
     }
 }

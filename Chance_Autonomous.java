f39cd803ac98583e34a8fe16d6f1a2aabcc9e240
RedLionRoboticsClub <32177555+RedLionRoboticsClub@users.noreply.github.com>
2017-11-29 14:42:35 -0500

    Update Chance_Autonomous.java

--- Chance_Autonomous.java
+++ Chance_Autonomous.java
@@ -1,237 +1,199 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
+/**
+ * This OpMode uses the common HardwareK9bot class to define the devices on the robot.
+ * All device access is managed through the HardwareK9bot class. (See this class for device names)
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a basic Tank Drive Teleop for the K9 bot
+ * It raises and lowers the arm using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claw slowly using the X and B buttons.
+ *
+ * Note: the configuration of the servos is such that
+ * as the arm servo approaches 0, the arm position moves up (away from the floor).
+ * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
 
-@Autonomous(name="Chance Autonomous", group ="TestsOpMode")
+@TeleOp(name="AnthonyWillBeMad", group="Test")
 
+public class AnthonyWillBeMad extends LinearOpMode {
 
-public class Chance_Autonomous extends LinearOpMode
-{
-
-    DcMotor motorBR;
-    DcMotor motorBL;
-    DcMotor motorFL;
-    DcMotor motorFR;
-//    private static final int MOTOR_TICKS = 1220;
-    // in cm
-//    private static final double WHEEL_CIRCUMFERENCE = 32.5;
-//    private static final double ROBOT_WIDTH = 37.5;
-//    private static final double ROBOT_LENGTH = 37.5;
+    /* Declare OpMode members. */
+    HardwareK9bot2 robot           = new HardwareK9bot2();              // Use a K9'shardware
+    double          armPosition     = robot.ARM_HOME;                   // Servo safe position
+    //   double          clawPosition    = robot.CLAW_HOME;                  // Servo safe position
+    //  final double    CLAW_SPEED      = 0.01 ;                            // sets rate to move servo
+    final double    ARM_SPEED       = 0.01 ;                            // sets rate to move servo
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+        double left;
+        double right;
 
-        motorBL  = this.hardwareMap.dcMotor.get("motorBL");
-        motorBR = this.hardwareMap.dcMotor.get("motorBR");
-        motorFL = this.hardwareMap.dcMotor.get("motorFL");
-        motorFR = this.hardwareMap.dcMotor.get("motorFR");
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
 
-        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
 
-        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // 37 cm from center of front wheels to 91 cm minus radius (half of width, 19 cm)
-        moveMotors(.5,.5,.5,.5,2000,2000,2000,2000);
-        reverseMotors(.5,.5,.5,.5,2000,2000,2000,2000);
-        rightMotors(.5,.5,.5,.5,2000,2000,2000,2000);
-        leftMotors(.5,.5,.5,.5,2000,2000,2000,2000);
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
 
-        //write thing here to make
-        //moveMotors(1/4, 0, (int) Math.round(ROBOT_WIDTH*Math.PI/4), 0);
+            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+            if (gamepad1.right_trigger > 0) {
 
-    }
+                robot.motorFR.setPower(-0.5);
+                robot.motorFL.setPower(0.5);
+                robot.motorBL.setPower(-0.5);
+                robot.motorBR.setPower(0.5);
+            }
 
-    /**
-     * Converts a distance of centimeters into ticks for the encoders of the motors
-     *
-     * @param cm the distance in centimeters
-     * @return a rounded value of the equivalent amount of distance in ticks
-     */
-//    int distance (double cm)
+            else if (gamepad1.left_trigger > 0) {
+                robot.motorFR.setPower(0.5);
+                robot.motorFL.setPower(-0.5);
+                robot.motorBL.setPower(0.5);
+                robot.motorBR.setPower(-0.5);
+            }
 
+            else {
+                float yValue = gamepad1.left_stick_y;
 
-    /**
-     * tells the motors to move at a certain speed (power) and to a specific distance in cm
-     *
-     * @param powerBL the amount of power that the left motor should use [-1.0, 1.0]
-     * @param powerBR the amount of power that the right motor should use [-1.0, 1.0]
-     * @param posBL the amount in centimeters that the left wheel should turn
-     * @param posBR the amount in centimeters that the right wheel should turn
-     * @throws InterruptedException
-     */
-    void moveMotors (double powerBL, double powerBR, double powerFL, double powerFR,
-                     int posBL, int posBR, int posFL, int posFR) throws InterruptedException
-    {
-        motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+                float xValue = gamepad1.right_stick_y;
 
-        motorBL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorBR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        motorBL.setTargetPosition(posBL);
-        motorBR.setTargetPosition(posBR);
-        motorFR.setTargetPosition(posFR);
-        motorFL.setTargetPosition(posFL);
-
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ///the usadjieiwebiqeuhowfeodfqwiwewenqwnrnower jkdfnasikjfmxbnc liawekjsmhdbf iukjqd
 
 
-        motorBL.setPower(powerBL);
-        motorBR.setPower(powerBR);
-        motorFR.setPower(powerFR);
-        motorFL.setPower(powerFL);
-        // BUSYWAIT... Don't show this to our CS professors :S
-        while (motorFL.getCurrentPosition() < posFL || motorFR.getCurrentPosition() < posFR || motorBL.getCurrentPosition() < posBL || motorBR.getCurrentPosition() < posBR)
-        {
-            Thread.sleep(20);
+                xValue = Range.clip(xValue, -1, 1);
+                yValue = Range.clip(yValue, -1, 1);
+
+                if (gamepad1.left_bumper) {
+                    robot.motorFL.setPower(-yValue/2);
+                    robot.motorBL.setPower(-yValue/2);
+                    robot.motorFR.setPower(-xValue/2);
+                    robot.motorBR.setPower(-xValue/2);
+                }
+
+                else if (gamepad1.right_bumper) {
+                    robot.motorFL.setPower(-yValue/4);
+                    robot.motorBL.setPower(-yValue/4);
+                    robot.motorFR.setPower(-xValue/4);
+                    robot.motorBR.setPower(-xValue/4);
+                }
+
+                else if (gamepad1.dpad_down)  {
+                    robot.motorFL.setPower(-.25);
+                    robot.motorBL.setPower(-.25);
+                    robot.motorFR.setPower(-.25);
+                    robot.motorBR.setPower(-.25);
+                }
+
+                else if (gamepad1.dpad_up)  {
+                    robot.motorFL.setPower(.25);
+                    robot.motorBL.setPower(.25);
+                    robot.motorFR.setPower(.25);
+                    robot.motorBR.setPower(.25);
+                }
+
+                else if (gamepad1.dpad_right)  {
+                    robot.motorFR.setPower(-0.5);
+                    robot.motorFL.setPower(0.5);
+                    robot.motorBL.setPower(-0.5);
+                    robot.motorBR.setPower(0.5);
+                }
+
+                else if (gamepad1.dpad_left)  {
+                    robot.motorFR.setPower(0.5);
+                    robot.motorFL.setPower(-0.5);
+                    robot.motorBL.setPower(0.5);
+                    robot.motorBR.setPower(-0.5);
+                }
+
+                else {
+                    robot.motorFL.setPower(-yValue);
+                    robot.motorBL.setPower(-yValue);
+                    robot.motorFR.setPower(-xValue);
+                    robot.motorBR.setPower(-xValue);
+                }
+
+            }
+            if (gamepad2.left_bumper) {
+                if (gamepad2.a) {
+                    robot.motorArm.setPower(-0.5);
+                } else if (gamepad2.b) {
+                    robot.motorArm.setPower(0.5);
+                } else {
+                    robot.motorArm.setPower(0);
+                }
+            }
+
+            else if (gamepad2.right_bumper) {
+                if (gamepad2.a) {
+                    robot.motorArm.setPower(-0.25);
+                } else if (gamepad2.b) {
+                    robot.motorArm.setPower(0.25);
+                } else {
+                    robot.motorArm.setPower(0);
+                }
+            }
+
+            else {
+                if (gamepad2.a) {
+                    robot.motorArm.setPower(-1);
+                } else if (gamepad2.b) {
+                    robot.motorArm.setPower(1);
+                } else {
+                    robot.motorArm.setPower(0);
+                }
+            }
+
+
+
+
+            // Use gamepad Y & A raise and lower the arm
+            // if (gamepad1.right_bumper)
+            // armPosition += ARM_SPEED;
+            // else if (gamepad1.left_bumper)
+            // armPosition -= ARM_SPEED;
+
+            // Use gamepad X & Y to open and close the claw
+            if (gamepad2.x) {
+                armPosition += ARM_SPEED;
+            }
+            else if (gamepad2.y) {
+                armPosition -= ARM_SPEED;
+            }
+
+            // Move both servos to new position.
+            armPosition  = Range.clip(armPosition, robot.ARM_MIN_RANGE, robot.ARM_MAX_RANGE);
+            robot.arm.setPosition(armPosition);
+            // clawPosition = Range.clip(clawPosition, robot.CLAW_MIN_RANGE, robot.CLAW_MAX_RANGE);
+            // robot.claw.setPosition(clawPosition);
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("arm",   "%.2f", armPosition);
+            // telemetry.addData("claw",  "%.2f", clawPosition);
+            telemetry.update();
+
+            // Pause for 40 mS each cycle = update 25 times a second.
+            sleep(40);
+
         }
-        /**      motorBR.setPower(0.0);
-         motorBL.setPower(0.0);
-         motorFL.setPower(0);
-         motorFR.setPower(0);
-         motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-         motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-         motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-         motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);             **/
-        // target has reached destination
-//        motorLeft.setPower(0.0);
-//        motorRight.setPower(0.0);
-//        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
 
-
-    void rightMotors(double powerBL, double powerBR, double powerFL, double powerFR,
-                     int posBL, int posBR, int posFL, int posFR) throws InterruptedException{
-        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        motorBL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorBR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        motorBL.setTargetPosition(posBL);
-        motorBR.setTargetPosition(posBR);
-        motorFR.setTargetPosition(posFR);
-        motorFL.setTargetPosition(posFL);
-
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-        motorBL.setPower(powerBL);
-        motorBR.setPower(powerBR);
-        motorFR.setPower(powerFR);
-        motorFL.setPower(powerFL);
-        // BUSYWAIT... Don't show this to our CS professors :S
-        while (motorFL.getCurrentPosition() < posFL || motorFR.getCurrentPosition() < posFR || motorBL.getCurrentPosition() < posBL || motorBR.getCurrentPosition() < posBR)
-        {
-            Thread.sleep(20);
-        }
-    }
-
-    void leftMotors(double powerBL, double powerBR, double powerFL, double powerFR,
-                    int posBL, int posBR, int posFL, int posFR) throws InterruptedException{
-        motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        motorBL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorBR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        motorBL.setTargetPosition(posBL);
-        motorBR.setTargetPosition(posBR);
-        motorFR.setTargetPosition(posFR);
-        motorFL.setTargetPosition(posFL);
-
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-        motorBL.setPower(powerBL);
-        motorBR.setPower(powerBR);
-        motorFR.setPower(powerFR);
-        motorFL.setPower(powerFL);
-        // BUSYWAIT... Don't show this to our CS professors :S
-        while (motorFL.getCurrentPosition() < posFL || motorFR.getCurrentPosition() < posFR || motorBL.getCurrentPosition() < posBL || motorBR.getCurrentPosition() < posBR)
-        {
-            Thread.sleep(20);
-        }
-    }
-
-
-    void reverseMotors(double powerBL, double powerBR, double powerFL, double powerFR,
-                       int posBL, int posBR, int posFL, int posFR) throws InterruptedException{
-        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        motorBL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorBR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFR.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        motorFL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        motorBL.setTargetPosition(posBL);
-        motorBR.setTargetPosition(posBR);
-        motorFR.setTargetPosition(posFR);
-        motorFL.setTargetPosition(posFL);
-
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-        motorBL.setPower(powerBL);
-        motorBR.setPower(powerBR);
-        motorFR.setPower(powerFR);
-        motorFL.setPower(powerFL);
-        // BUSYWAIT... Don't show this to our CS professors :S
-        while (motorFL.getCurrentPosition() < posFL || motorFR.getCurrentPosition() < posFR || motorBL.getCurrentPosition() < posBL || motorBR.getCurrentPosition() < posBR)
-        {
-            Thread.sleep(20);
-        }
-    }
-    // assumes power is in a step of a tenth...
-    void stopMotors () throws InterruptedException
-    {
-        double initpowerL = motorBL.getPower() * 10;
-        double initpowerR = motorBR.getPower() * 10;
-        for (int i = 0; i < Math.max(Math.abs(initpowerL), Math.abs(initpowerR)); i++)
-        {
-            int signLeft = (motorBL.getPower() < 0)? -1 : 1;
-            int signRight = (motorBR.getPower() < 0)? -1 : 1;
-            motorBL.setPower(signLeft * Math.abs(motorBL.getPower()) - 0.1);
-            motorBR.setPower(signRight * Math.abs(motorBR.getPower()) - 0.1);
-            Thread.sleep(50);
-        }
     }
 
 }

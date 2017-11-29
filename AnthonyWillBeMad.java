7075fd49eb86460d3239d7cbdccbfc2ea9cd5cbc
RedLionRoboticsClub <32177555+RedLionRoboticsClub@users.noreply.github.com>
2017-11-29 14:45:14 -0500

    Update AnthonyWillBeMad.java

--- AnthonyWillBeMad.java
+++ AnthonyWillBeMad.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package org.firstinspires.ftc.teamcode;
 
 
@@ -56,10 +28,10 @@ import com.qualcomm.robotcore.util.Range;
 public class AnthonyWillBeMad extends LinearOpMode {
 
     /* Declare OpMode members. */
-   HardwareK9bot2 robot           = new HardwareK9bot2();              // Use a K9'shardware
+    HardwareK9bot2 robot           = new HardwareK9bot2();              // Use a K9'shardware
     double          armPosition     = robot.ARM_HOME;                   // Servo safe position
- //   double          clawPosition    = robot.CLAW_HOME;                  // Servo safe position
-  //  final double    CLAW_SPEED      = 0.01 ;                            // sets rate to move servo
+    //   double          clawPosition    = robot.CLAW_HOME;                  // Servo safe position
+    //  final double    CLAW_SPEED      = 0.01 ;                            // sets rate to move servo
     final double    ARM_SPEED       = 0.01 ;                            // sets rate to move servo
 
     @Override
@@ -123,6 +95,34 @@ public class AnthonyWillBeMad extends LinearOpMode {
                     robot.motorBR.setPower(-xValue/4);
                 }
 
+                else if (gamepad1.dpad_down)  {
+                    robot.motorFL.setPower(-.25);
+                    robot.motorBL.setPower(-.25);
+                    robot.motorFR.setPower(-.25);
+                    robot.motorBR.setPower(-.25);
+                }
+
+                else if (gamepad1.dpad_up)  {
+                    robot.motorFL.setPower(.25);
+                    robot.motorBL.setPower(.25);
+                    robot.motorFR.setPower(.25);
+                    robot.motorBR.setPower(.25);
+                }
+
+                else if (gamepad1.dpad_right)  {
+                    robot.motorFR.setPower(-0.5);
+                    robot.motorFL.setPower(0.5);
+                    robot.motorBL.setPower(-0.5);
+                    robot.motorBR.setPower(0.5);
+                }
+
+                else if (gamepad1.dpad_left)  {
+                    robot.motorFR.setPower(0.5);
+                    robot.motorFL.setPower(-0.5);
+                    robot.motorBL.setPower(0.5);
+                    robot.motorBR.setPower(-0.5);
+                }
+
                 else {
                     robot.motorFL.setPower(-yValue);
                     robot.motorBL.setPower(-yValue);
@@ -130,7 +130,7 @@ public class AnthonyWillBeMad extends LinearOpMode {
                     robot.motorBR.setPower(-xValue);
                 }
 
-                }
+            }
             if (gamepad2.left_bumper) {
                 if (gamepad2.a) {
                     robot.motorArm.setPower(-0.5);

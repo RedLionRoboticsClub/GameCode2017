d397e6bbee6e04f9867858103e7d46aac41afd9e
RedLionRoboticsClub <32177555+RedLionRoboticsClub@users.noreply.github.com>
2018-01-05 21:37:14 -0500

    Update HardwareK9bot2.java

--- HardwareK9bot2.java
+++ HardwareK9bot2.java
@@ -27,6 +27,35 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -70,10 +99,10 @@ public class HardwareK9bot2
     public final static double ARM_HOME2 = 0.6;
     public final static double ARM_HOMEJ = 1;
     //public final static double CLAW_HOME = 0.2;
-    public final static double ARM_MIN_RANGE  = 0.3;
-    public final static double ARM_MIN_RANGE2  = .3;
-    public final static double ARM_MAX_RANGE  = 0.6;
-    public final static double ARM_MAX_RANGE2  = 0.60;
+    public final static double ARM_MIN_RANGE  = 0;
+    public final static double ARM_MIN_RANGE2  = 0;
+    public final static double ARM_MAX_RANGE  = 1;
+    public final static double ARM_MAX_RANGE2  = 1;
     public final static double ARM_MIN_RANGEJ  = 0;
     public final static double ARM_MAX_RANGEJ  = 0.90;
     //public final static double CLAW_MIN_RANGE  = 0.20;
@@ -130,4 +159,3 @@ public class HardwareK9bot2
 //        claw.setPosition(CLAW_HOME);
     }
 }
-
